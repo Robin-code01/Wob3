6f072a886ca8e8832eaaa752bf749f2a56a3ad2b
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-07-25 02:04:12 +1200

    fix: update redirect in Home component and change AuthHeader link to root

--- frontend/src/components/layout/auth-header.tsx
+++ frontend/src/components/layout/auth-header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function AuthHeader() {
   return (
     <header className="bg-primary py-4 px-8">
-      <Link href="/home" className="text-background text-2xl">
+      <Link href="/" className="text-background text-2xl">
         [ N A M E ]
       </Link>
     </header>
