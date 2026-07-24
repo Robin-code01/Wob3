6d9d02dd4b1355f7351e62805ff4fefdb8658941
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-07-24 23:01:21 +1200

    Refactor Providers component to integrate SessionProvider and RainbowKitSiweNextAuthProvider for improved authentication handling and update tsconfig paths for better module resolution.

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -5,17 +5,23 @@ import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
 import { config } from "../lib/auth/config";
 import { sepolia } from "wagmi/chains";
+import { RainbowKitSiweNextAuthProvider } from "@rainbow-me/rainbowkit-siwe-next-auth";
+import { SessionProvider } from "next-auth/react";
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider initialChain={sepolia}>
-          {children}
-        </RainbowKitProvider>
-      </QueryClientProvider>
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitSiweNextAuthProvider>
+            <RainbowKitProvider initialChain={sepolia}>
+              {children}
+            </RainbowKitProvider>
+          </RainbowKitSiweNextAuthProvider>
+        </QueryClientProvider>
+      </SessionProvider>
     </WagmiProvider>
   );
 }
