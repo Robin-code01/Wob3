6c63f579329368854697a85fe02eb6d5ad6b7f84
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-07-25 10:56:24 +1200

    Remove unused image import from Home page component

--- frontend/src/app/(main)/home/page.tsx
+++ frontend/src/app/(main)/home/page.tsx
@@ -1,10 +1,8 @@
 import CourseCard from "@/components/card/course-card";
-import fish from "../../../../public/IMG_1096.jpeg";
 
 export default function Home() {
   return (
     <CourseCard
-      src={fish}
       title="COSC122"
       author="Daigo KItagawa"
       description="Joy to the world!"
