4329df41a8b0306d2fe8fcc37bdb2568ab56ff30
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-07-25 12:14:03 +1200

    Integrate FillInTheBlankQuestion into QuizPage for enhanced quiz functionality

--- frontend/src/app/(main)/courses/[course_id]/[module_id]/page.tsx
+++ frontend/src/app/(main)/courses/[course_id]/[module_id]/page.tsx
@@ -2,6 +2,7 @@
 
 import QuizBox from "@/features/quiz/quiz-box";
 import QuizQuestion from "@/features/quiz/multiple-choice";
+import FillInTheBlankQuestion from "@/features/quiz/fill-in-the-blank";
 
 export default function QuizPage() {
   const sampleOptions = [
@@ -15,13 +16,27 @@ export default function QuizPage() {
     console.log("Selected answer:", option);
   };
 
+  const handleAnswerChange = (userAnswer: string) => {
+    console.log("Current answer:", userAnswer);
+  };
+
   return (
-    <QuizBox>
-      <QuizQuestion
-        question="Which framework are you using for this project?"
-        options={sampleOptions}
-        onSelectOption={handleAnswer}
-      />
-    </QuizBox>
+    <>
+      <QuizBox>
+        <QuizQuestion
+          question="Which framework are you using for this project?"
+          options={sampleOptions}
+          onSelectOption={handleAnswer}
+        />
+      </QuizBox>
+      <QuizBox>
+        <FillInTheBlankQuestion
+          question="Next.js App Router uses Server Components by ___."
+          placeholder="e.g. default"
+          onAnswerSubmit={handleAnswerChange}
+        />
+      </QuizBox>
+      {/*I need to remember to make the answers get sent to the DB after every check of a question. Or if the answer is going to be sent all at once, the answers should be stored in a list and manipulated to be correctly stored in DB. What do you think?*/}
+    </>
   );
 }
